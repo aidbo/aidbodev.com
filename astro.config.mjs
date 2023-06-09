76b29a251f1dc918db56c3c7b698a5b22630f4d5
Zheng, Bang-Bo <zhengbangbo@hotmail.com>
2023-06-09 21:50:03 +0800

    feat: add robots.txt and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,18 @@
 import { defineConfig } from 'astro/config';
 import astroI18next from "astro-i18next";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [astroI18next()],
+  site: 'https://aidbodev.com',
+  integrations: [astroI18next(), sitemap({
+    i18n: {
+      defaultLocale: 'en',   // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
+      locales: {
+        en: 'en-US',         // The `defaultLocale` value must present in `locales` keys
+        zh: 'zh-CN',
+      },
+    },
+  })]
 });
